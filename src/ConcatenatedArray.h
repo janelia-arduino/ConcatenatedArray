8b5f4f6bdde39f85f9341a85a64f60a0e467eaa4
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 13:00:04 -0400

    Update email address

--- src/ConcatenatedArray.h
+++ src/ConcatenatedArray.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef CONCATENATED_ARRAY_H
 #define CONCATENATED_ARRAY_H
